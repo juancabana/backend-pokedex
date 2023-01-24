43e60efefd100f1b507257ca419783984a2ae9b4
juancabana <juand.cabanat@unac.edu.co>
2023-01-24 18:22:13 -0500

    service structure

--- routes/pokemon.router.js
+++ routes/pokemon.router.js
@@ -1,92 +1,105 @@
 import express from "express";
+import PokemonService from "../services/pokemon.service.js";
 
+const service = new PokemonService();
 const router = express.Router();
 
 // Create pokemon
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    res.json({ message: "Create pokemon" });
+    const { data } = req.params;
+    const newPokemon = await service.createPokemon(data);
+    res.json(newPokemon);
   } catch (error) {
     next(error);
   }
 });
 
 // Get all pokemons
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.json({ message: "Get all pokemons" });
+    const pokemons = await service.findAll();
+    res.json(pokemons);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by ID
-router.get("/id/:id", (req, res) => {
+router.get("/id/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    res.json({ message: `Get pokemon by ID - ${id}` });
+    const pokemon = await service.findById(id);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by name
-router.get("/name/:name", (req, res) => {
+router.get("/name/:name", async (req, res) => {
   try {
     const { name } = req.params;
-    res.json({ message: `Get pokemon by name - ${name}` });
+    const pokemon = await service.findByName(name);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by ability
-router.get("/ability/:ability", (req, res) => {
+router.get("/ability/:ability", async (req, res) => {
   try {
     const { ability } = req.params;
-    res.json({ message: `Get pokemon by ability - ${ability}` });
+    const pokemon = await service.findByAbility(ability);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by weight
-router.get("/weight/:weight", (req, res) => {
+router.get("/weight/:weight", async (req, res) => {
   try {
     const { weight } = req.params;
-    res.json({ message: `Get pokemon by weight - ${weight}` });
+    const pokemon = await service.findByWeight(weight);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by speed
-router.get("/speed/:speed", (req, res) => {
+router.get("/speed/:speed", async (req, res) => {
   try {
     const { speed } = req.params;
-    res.json({ message: `Get pokemon by speed - ${speed}` });
+    const pokemon = await service.findBySpeed(speed);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by attack
-router.get("/attack/:attack", (req, res) => {
+router.get("/attack/:attack", async (req, res) => {
   try {
     const { attack } = req.params;
-    res.json({ message: `Get pokemon by attack - ${attack}` });
+    const pokemon = await service.findByAttack(attack);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by defense
-router.get("/defense/:defense", (req, res) => {
+router.get("/defense/:defense", async (req, res) => {
   try {
     const { defense } = req.params;
-    res.json({ message: `Get pokemon by defense - ${defense}` });
+    const pokemon = await service.findByDefense(defense);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
 });
 // Get pokemon by category
-router.get("/category/:category", (req, res) => {
+router.get("/category/:category", async (req, res) => {
   try {
     const { category } = req.params;
-    res.json({ message: `Get pokemon by category - ${category}` });
+    const pokemon = await service.findByCategory(category);
+    res.json(pokemon);
   } catch (error) {
     next(error);
   }
---
+++ services/pokemon.service.js
@@ -0,0 +1,42 @@
+class PokemonService {
+  constructor() {}
+  async createPokemon(pokemon) {
+    return { message: "Create pokemon" };
+  }
+  async findAll() {
+    return { message: "Get all pokemons" };
+  }
+  async findById(id) {
+    return { message: `Get pokemon by ID - ${id}` };
+  }
+
+  async findByName(name) {
+    return { message: `Get pokemon by name - ${name}` };
+  }
+
+  async findByAbility(ability) {
+    return { message: `Get pokemon by ability - ${ability}` };
+  }
+
+  async findByWeight(weight) {
+    return { message: `Get pokemon by weight - ${weight}` };
+  }
+
+  async findBySpeed(speed) {
+    return { message: `Get pokemon by speed - ${speed}` };
+  }
+
+  async findByAttack(attack) {
+    return { message: `Get pokemon by attack - ${attack}` };
+  }
+
+  async findByDefense(defense) {
+    return { message: `Get pokemon by defense - ${defense}` };
+  }
+
+  async findByCategory(category) {
+    return { message: `Get pokemon by category - ${category}` };
+  }
+}
+
+export default PokemonService;
